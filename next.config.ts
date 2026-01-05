2429d7acdf443d8d3352f47854bfd0e3a11449d9
Vishal Shukla <vishalshukla@Vishals-MacBook-Pro.local>
2026-01-05 19:12:46 +0530

    Add standalone output for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
